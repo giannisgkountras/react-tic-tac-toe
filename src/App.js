cfa3a577c49fdc759e331fda41a68fc1b048e60b
giannisgkountras <gkountrasioannis@gmail.com>
2022-10-12 23:15:43 +0300

    base setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Box from "./components/Box/Box";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="board-container">
+          <div className="row">
+            <Box />
+            <Box />
+            <Box />
+          </div>
+          <div className="row">
+            <Box />
+            <Box />
+            <Box />
+          </div>
+          <div className="row">
+            <Box />
+            <Box />
+            <Box />
+          </div>
+        </div>
       </header>
     </div>
   );
---
+++ src/components/Xmark/Xmark.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Xmark.css";
+
+export default function Xmark() {
+  return <div>Xmark</div>;
+}
